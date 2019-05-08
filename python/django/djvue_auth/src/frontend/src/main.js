79e58532da5a42914068948c2c93badf0d3269ef
Sergey Lagovskiy <slagovskiy@gmail.com>
2019-05-08 14:10:26 +0700

    🔨

--- python/django/djvue_auth/src/frontend/src/main.js
+++ python/django/djvue_auth/src/frontend/src/main.js
@@ -4,19 +4,11 @@ import Vuelidate from 'vuelidate'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import Axios from 'axios'
 
 import App from './App.vue'
 import store from './store/index'
 import router from './routes'
 
-Vue.prototype.$http = Axios;
-Axios.defaults.xsrfCookieName = 'csrftoken'
-Axios.defaults.xsrfHeaderName = 'X-CSRFToken'
-const accessToken = localStorage.getItem('access_token')
-if (accessToken) {
-    Vue.prototype.$http.defaults.headers.common['Authorization'] = accessToken
-}
 
 Vue.use(VueRouter)
 Vue.use(Vuetify, {
