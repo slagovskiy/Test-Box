b645b40d45feee6907820f729dd2c48e1f6a5931
Sergey Lagovskiy <slagovskiy@gmail.com>
2019-08-21 16:39:25 +0700

    💩

---
+++ asp-core/teach01/teach01/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using teach01.Models;
+
+namespace teach01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int h = DateTime.UtcNow.Hour;
+            ViewBag.Greating = h < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult InviteForm() {
+            return View();
+        }
+
+        [HttpPatch]
+        public ViewResult InviteForm(GuestResponse guestResponse) {
+            return View();
+        }
+        
+    }
+}
