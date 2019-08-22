04cb1ec0286aaa9b03fc991af4877d67304a5c0c
Sergey Lagovskiy <slagovskiy@gmail.com>
2019-08-22 16:04:34 +0700

    💩

---
+++ asp-core/teach02/teach02/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using teach02.Models;
+
+namespace teach02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View(new string[] { "C#", "Language", "Tests" });
+            List<string> prod = new List<string>();
+            foreach (Product p in Product.GetProducts()) {
+                string name = p?.Name ?? "<None>";
+                decimal? price = p?.Price;
+                string related = p?.Related?.Name ?? "<None>";
+                prod.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, related));
+                prod.Add($"Name: {name}, Price: {price}, Related: {related}");
+            }
+            return View(prod);
+        }
+    }
+}
---
+++ asp-core/teach02/teach02/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teach02.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+
+        public static Product[] GetProducts() {
+            Product car = new Product
+            {
+                Name = "Mersedes 600",
+                Price = 100M
+            };
+            Product jacket = new Product
+            {
+                Name = "Malinovii",
+                Price = 50M
+            };
+
+            car.Related = jacket;
+
+            return new Product[] { car, jacket, null };
+        }
+    }
+}
