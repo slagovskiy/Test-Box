e60f77bbeb236fcc09c0a7764eaebe859b51513f
Sergey Lagovskiy <slagovskiy@gmail.com>
2019-08-23 15:36:59 +0700

    💩

---
+++ asp-core/teach03/teach03/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using teach03.Models;
+
+namespace teach03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Mersedes",
+                Description = "Cool 600 model",
+                Category = "car",
+                Price = 100M
+            };
+            ViewBag.StockLevel = 2;
+            return View(myProduct);
+        }
+    }
+}
