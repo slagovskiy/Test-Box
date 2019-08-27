88c2ee1365501de26b84ce22cc6afd3c9a67d451
Sergey Lagovskiy <slagovskiy@gmail.com>
2019-08-27 16:43:00 +0700

    ⚙

---
+++ asp-core/teach04/teach04/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teach04.Models
+{
+    public class FakeProductRepository: IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Surfboard", Price = 179 },
+            new Product { Name = "Running shoes", Price = 300 }
+        }.AsQueryable<Product>();
+    }
+}
