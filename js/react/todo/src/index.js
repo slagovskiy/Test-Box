6ba4c252424b84d3a83b820043e0c6f98d6f903a
Sergey Lagovskiy <slagovskiy@gmail.com>
2020-08-21 16:10:01 +0700

    simple components

--- js/react/todo/src/index.js
+++ js/react/todo/src/index.js
@@ -1,1 +1,41 @@
-console.log('123')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    )
+}
+
+const AppHeader = () => {
+    return (
+        <h1>Todo app</h1>
+    )
+}
+
+const SearchPanel = () => {
+    return (
+        <input placeholder="search"/>
+    )
+}
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>123</li>
+            <li>456</li>
+            <li>789</li>
+        </ul>
+    )
+}
+
+const el = (
+    <App/>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
+
