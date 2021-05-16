829b09e1b4aaba424d8e5ea4cc97f2e2203489d1
Sergey Lagovskiy <slagovskiy@gmail.com>
2021-05-17 00:48:26 +0700

    ⛏

---
+++ asp-core/core-react/Persistence/Migrations/20210514084535_ActivityFix.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class ActivityFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sity",
+                table: "Activities",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "Desctiption",
+                table: "Activities",
+                newName: "City");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "Sity");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "Activities",
+                newName: "Desctiption");
+        }
+    }
+}
